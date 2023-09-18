7f7443dc8b2a08782a88ca337b0c1ff398e6078e
aron <114922035+aronpaco@users.noreply.github.com>
2023-09-18 21:23:21 +0300

    kodutoo initial

---
+++ kodutood/0920/basics.js
@@ -0,0 +1,7 @@
+/* Tutvuda uuesti tunni ajal omandatud ülesannetega ja koduste tööde kaustas võib teha sama basics.js, kus proovite läbi funktsioonid, arrow funktsioonid, shorthand arrow funktsioonid, string literals, object literals, spread syntax, map/filter + proovige läbi ka reduce funktsioon. */
+
+// function
+function findSquare(number) {
+    return number**2;
+}
+console.log(findSquare(5));
