c19f7c12689f110f19b5293ee4199cc9f0095ea9
aron <114922035+aronpaco@users.noreply.github.com>
2023-09-19 15:16:57 +0300

    basics.js

--- kodutood/0920/basics.js
+++ kodutood/0920/basics.js
@@ -4,4 +4,36 @@
 function findSquare(number) {
     return number**2;
 }
-console.log(findSquare(5));
+// console.log(findSquare(5));
+
+// arrow function
+const findSquare2 = (number) => {
+    return number**2;
+}
+// console.log(findSquare2(6));
+
+// shorthand? arrow function
+const findSquare3 = (number) => number**2;
+// console.log(findSquare3(7));
+
+// String literals
+const name = "Aron";
+// console.log(`Hello, ${name}`);
+
+// Object literals
+
+
+// Spread syntax
+
+
+// Map
+const numberArray = [0,1,2,3,4,5,6,7,8,9];
+const squareArray = numberArray.map(element => element**2);
+// console.log({squareArray});
+
+// Filter
+const evenArray = numberArray.filter(element => element % 2 == 0);
+const oddArray = numberArray.filter(element => element % 2);
+// console.log({evenArray}, {oddArray});
+
+// Reduce
