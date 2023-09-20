1c00ab33d8cbb729c5c3c34da14c9a82f47d9610
aron <114922035+aronpaco@users.noreply.github.com>
2023-09-20 15:52:55 +0300

    tunnitoo readme koos lingiga

---
+++ tunnitood/0920/quokka.js
@@ -0,0 +1,25 @@
+let array = [1, 2, 3, 4, 47]
+let b = array[4]
+console.log(b)
+/*
+const deconstruct = () => {
+  const [b, , c, , a] = array()
+  return { a, b, c }
+}
+console.log(deconstruct())
+*/
+
+
+;(() => {   fn(...).callback is not a function
+    const functionCaller = fn => {
+        if (!fn().callback)
+}})
+
+const raimo = {
+  name: "Raimo",
+  school: "TLU",
+  ":DD :DD": "Koolon dee"
+}
+console.log(raimo.name + " " + raimo.school)
+console.log(raimo["name"] + " " + raimo["school"])
+console.log(raimo[":DD :DD"])
---
+++ tunnitood/0920/readme.md
@@ -0,0 +1,1 @@
+https://github.com/aronpaco/super-duper-waffle
